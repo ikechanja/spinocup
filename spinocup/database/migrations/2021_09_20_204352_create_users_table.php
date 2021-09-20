a828525142b8e0065a1ae995f44b74b40d0b3f3d
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-21 00:03:45 +0900

    データベースのユニーク化

--- spinocup/database/migrations/2021_09_20_204352_create_users_table.php
+++ spinocup/database/migrations/2021_09_20_204352_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email', 255);
-            $table->string('name', 255);
-            $table->string('passowrd', 255);
+            $table->string('email', 255)->unique();
+            $table->string('name', 255)->unique();
+            $table->string('password', 255);
             $table->text('profile');
             $table->timestamps();
         });
