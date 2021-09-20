b6277629970623a031badbbf322f9df81a038116
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-20 17:28:24 +0900

    キャッチコピー作成

--- spinocup/resources/views/top.blade.php
+++ spinocup/resources/views/top.blade.php
@@ -1,5 +1,14 @@
 @include('header')
 <section class="top-wrapper">
     <div class="bg-mask">
+        <div class="container">
+            <div class="top-impact">
+                <div>
+                    <h1 class="top-wrapper-h1">思い出を語ろう</h1>
+                    <h1 class="top-wrapper-h1">思い出に浸ろう</h1>
+                    <h2 class="top-wrapper-h2">OMOIDE CHATは、夏休み最後に開かれる、特別な祭典です。</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
