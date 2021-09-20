fec9d4615c7e12bc0699df3a0da11821c5c62101
kitiumi <kuaidouheiyu3@gmail.com>
2021-09-20 18:35:15 +0900

    Update welcome.blade.php
    !!!!!!

--- spinocup/resources/views/welcome.blade.php
+++ spinocup/resources/views/welcome.blade.php
@@ -429,7 +429,7 @@
                     </g>
                 </svg>
             </div>
-            <p>スピノカップ！????</p>
+            <p>スピノカップ！????!!!!!</p>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
