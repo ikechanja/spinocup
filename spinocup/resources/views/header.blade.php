c618cc154da57dac12c8dca9516b900ff14b6e83
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-20 17:08:53 +0900

    ヘッダー作成

--- spinocup/resources/views/header.blade.php
+++ spinocup/resources/views/header.blade.php
@@ -5,7 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
-    <title>SFの巨塔</title>
+    <title>OMOIDE CHAT</title>
 </head>
+
+<body>
+    <header class="top-header">
+        <div class="container">
+            <h1 class="top-logo">OMOIDE CHAT</h1>
+        </div>
+    </header>
+</body>
