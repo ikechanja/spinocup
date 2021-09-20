2a965fe754246c1b8a6a198f30c8e02e4288fd8c
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-21 04:35:43 +0900

    バリデーションの表示

--- spinocup/resources/views/login_form.blade.php
+++ spinocup/resources/views/login_form.blade.php
@@ -7,8 +7,18 @@
             <form class="login-form" method="POST" action="{{route('login')}}">
                 @csrf
                 <p>メールアドレス</p>
+                @if ($errors->has('email'))
+                <div class="sign-error">
+                    {{ $errors->first('email') }}
+                </div>
+                @endif
                 <input class="login-input" type="email" name="email">
                 <p>パスワード</p>
+                @if ($errors->has('password'))
+                <div class="sign-error">
+                    {{ $errors->first('password') }}
+                </div>
+                @endif
                 <input class="login-input" type="password" name="password">
                 <input class="login-submit" type="submit" value="ログイン">
             </form>
