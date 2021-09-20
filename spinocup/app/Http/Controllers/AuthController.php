147fc6b46207704421aaf18defb4d4df979f1355
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-21 04:51:55 +0900

    AuthControllerの実装

--- spinocup/app/Http/Controllers/AuthController.php
+++ spinocup/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginFormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -21,6 +22,15 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        dd($request->all());
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect('home')->with('login_success', 'お帰りなさい！');
+        }
+
+        return back()->withErrors([
+            'login_error' => 'メールアドレスかパスワードが間違っています。',
+        ]);
     }
 }
