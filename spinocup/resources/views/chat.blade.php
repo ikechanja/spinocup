6a93fccc1a1803db2a1df543f4ddd7bac1e6f477
morikne36 <morikne11450@gmail.com>
2021-09-21 05:01:59 +0900

    chst-apper

--- spinocup/resources/views/chat.blade.php
+++ spinocup/resources/views/chat.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <head>
+<meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Pusher Test</title>
+  <meta name="csrf-token" content="{{csrf_token()}}">
+ <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
   <script>
 
@@ -16,12 +21,33 @@
       alert(JSON.stringify(data));
     });
   </script>
+   <style>
+  .list-group{
+   overflow-y: scroll;
+   height: 200px;
+  }
+ </style>
 
 </head>
 <body>
-  <h1>Pusher Test</h1>
+  <!-- <h1>Pusher Test</h1>
   <p>
     Try publishing an event to channel <code>my-channel</code>
     with event name <code>my-event</code>.
-  </p>
+  </p> -->
+  <div class="container">
+  <div class="row" id="app">
+   <h1>Chat room</h1>
+   <div class="offset-4 col-md-4">
+    <li class="list-group-item active">Chat</li>
+    <ul class="list-group">
+     <message v-for="value in chat.message">
+      @{{value}}
+     </message>
+    </ul>
+    <input type="text" class="form-control" placeholder="Type your message here.." v-model='message' @keyup.enter='send'>
+   </div>
+  </div>
+ </div>
+ <script src="{{ asset('js/app.js') }}"></script>
 </body>
