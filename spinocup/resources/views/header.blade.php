347fa907b8c74d344390642343aa43e4ec712dc9
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-20 16:59:42 +0900

    header.blade.php作成

---
+++ spinocup/resources/views/header.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+    <title>SFの巨塔</title>
+</head>
