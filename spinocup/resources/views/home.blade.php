9d5ecfb74e01334639acb02030dc7483180dcac5
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-21 15:30:39 +0900

    プロフィール編集リンクを追加

--- spinocup/resources/views/home.blade.php
+++ spinocup/resources/views/home.blade.php
@@ -8,6 +8,9 @@
             </div>
             @endif
             <p>お帰りなさい！{{Auth::user()->name}}</p>
+            <h1>ようこそ、OMODE CHATの世界へ</h1>
+            <a class="select-room-link" href="#">部屋を選びに行く</a>
+            <a class="change-profile-link" href="#">プロフィールを編集</a>
             <form action="{{route('logout')}}" method="POST">
                 @csrf
                 <button class="logout-btn">ログアウト</button>
