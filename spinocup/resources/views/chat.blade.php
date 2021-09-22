3485c373568791e04593029c029509ed9ed49a59
Shotaro Ikejiri <ikejiri.asana@gmail.com>
2021-09-22 10:43:52 +0900

    スタイリング調整

--- spinocup/resources/views/chat.blade.php
+++ spinocup/resources/views/chat.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
+
 <head>
-<meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Pusher Test</title>
   <meta name="csrf-token" content="{{csrf_token()}}">
   <link rel="stylesheet" type="text/css" href="/css/chat.css">
- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-  
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
 
 </head>
+
 <body>
   <!-- <h1>Pusher Test</h1>
   <p>
     Try publishing an event to channel <code>my-channel</code>
     with event name <code>my-event</code>.
   </p> -->
-  
+
   <!-- <div class="app">
   <div class="contents">
   <div class="top-left">
@@ -27,71 +29,70 @@
   </div>
    <div class="top-right">
     <li class="list-group-item active"># Chat room</li> -->
-    <div class="container">
-  <div class="row" id="app">
-   <h1>Chat room</h1>
-   <div class="offset-3 col-md-9">
-    <li class="list-group-item active">Chat</li>
-    <ul class="list-group"> 
-     <!-- <message v-for="value in chat.message">
+  <div class="container">
+    <div class="row" id="app">
+      <h1>Chat room</h1>
+      <div class="offset-3 col-md-9">
+        <li class="list-group-item active chat-header">OMOIDE CHAT</li>
+        <ul class="list-group">
+          <!-- <message v-for="value in chat.message">
       @{{value}}
      </message> -->
-     <table id="targetTable">
-    <tbody>
-        @for($i=0;$i<= 20;$i++)
-        <tr>
-            <td>{{$messageall[$i]->user->name}}</td>
-            <td>{{$messageall[$i]->message}}</td>
-        </tr>
-        @endfor
-    </tbody>
-    </table>
-    <!-- is_countable($messageall)-1 -->
-    </ul>
-    <input type="text" class="form-control" placeholder="Type your message here.." v-model='message' @keyup.enter='send' id="form1">
-   </div>
+          <table id="targetTable">
+            <tbody>
+              @for($i=0;$i<= 20;$i++) <tr>
+                <td>{{$messageall[$i]->user->name}}</td>
+                <td>{{$messageall[$i]->message}}</td>
+                </tr>
+                @endfor
+            </tbody>
+          </table>
+          <!-- is_countable($messageall)-1 -->
+        </ul>
+        <input type="text" class="form-control" placeholder="Type your message here.." v-model='message' @keyup.enter='send' id="form1">
+      </div>
+    </div>
   </div>
- </div>
- <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
 <script>
-    src="{{ asset('js/app.js') }}"
+  src = "{{ asset('js/app.js') }}"
 
-    //const usename = ''
+  //const usename = ''
 
-    window.Pusher = Pusher;
+  window.Pusher = Pusher;
 
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
 
-    var pusher = new Pusher('5860cc0ba59b0e5a95ee', {
-      cluster: 'ap3'
-    });
+  var pusher = new Pusher('5860cc0ba59b0e5a95ee', {
+    cluster: 'ap3'
+  });
 
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      //alert(JSON.stringify(data));
-      datas = JSON.stringify(data);
-      //alert(datas);
-      jsondata = JSON.parse(datas);
-      console.log(jsondata.message);
+  var channel = pusher.subscribe('my-channel');
+  channel.bind('my-event', function(data) {
+    //alert(JSON.stringify(data));
+    datas = JSON.stringify(data);
+    //alert(datas);
+    jsondata = JSON.parse(datas);
+    console.log(jsondata.message);
 
-      let table = document.getElementById('targetTable');
-      let newRow = table.insertRow();
+    let table = document.getElementById('targetTable');
+    let newRow = table.insertRow();
 
-      let newCell = newRow.insertCell();
-      let newText = document.createTextNode(jsondata.user);
-      newCell.appendChild(newText);
+    let newCell = newRow.insertCell();
+    let newText = document.createTextNode(jsondata.user);
+    newCell.appendChild(newText);
 
-      newCell = newRow.insertCell();
-      newText = document.createTextNode(jsondata.message);
-      newCell.appendChild(newText);
-      
-    });
-  </script>
-   <style>
-  .list-group{
-   overflow-y: scroll;
-   height: 400px;
+    newCell = newRow.insertCell();
+    newText = document.createTextNode(jsondata.message);
+    newCell.appendChild(newText);
+
+  });
+</script>
+<style>
+  .list-group {
+    overflow-y: scroll;
+    height: 400px;
   }
- </style>
+</style>
